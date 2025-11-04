b20a416958868735042ffb85d3e0edcdc23b1417
Milton Castro <miltondeniscastro.1993@gmail.com>
2025-11-04 09:50:25 -0300

    Freelance (2021) Code AA
    Juego para Android

---
+++ Assets/IKAnimation/IkWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkWeapon : MonoBehaviour {
+    public Animator anim;
+    public GameObject weapon;
+    private Vector3 posHand = Vector3.zero;
+    private Quaternion rotHand = Quaternion.identity;
+
+    private void Update() {
+        posHand = anim.GetBoneTransform(HumanBodyBones.LeftHand).position;
+        rotHand = anim.GetBoneTransform(HumanBodyBones.LeftHand).rotation;
+    }
+
+    private void LateUpdate() {
+        weapon.transform.position = posHand;
+        weapon.transform.rotation = rotHand;
+    }
+
+}
---
+++ Assets/IKAnimation/Particulas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particulas : MonoBehaviour
+{
+    private Vector3 posRelative = Vector3.zero;
+    public Animator anim;
+    private Vector3 _posBoneHand;
+    public GameObject myParticules;
+
+    private void Awake() {
+        anim.GetComponent<Animator>();
+        posRelative = myParticules.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        myParticules.transform.position = _posBoneHand;
+    }
+
+    private void LateUpdate() {
+        _posBoneHand = anim.GetBoneTransform(HumanBodyBones.LeftHand).position + posRelative - new Vector3(0.45f,0.2f,1.9f);
+    }
+
+}
